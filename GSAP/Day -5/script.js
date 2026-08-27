6e4fd97086794a1c0ae3a22aaa322ee4ae1f6f52
DevHarsh <harshituprety17@outlook.com>
2026-08-28 00:27:58 +0530

    text animation

--- GSAP/Day -5/script.js
+++ GSAP/Day -5/script.js
@@ -1,10 +1,36 @@
-let h1Text = document.querySelector("h1").textContent;
+function breakText() {
+  let h1 = document.querySelector("h1");
+  let h1Text = h1.textContent;
 
-let splitText = h1Text.split("");
-let cluster = "";
+  let splitText = h1Text.split("");
+  let cluster = "";
 
-splitText.forEach((e) => {
-  cluster += e;
+  let halfValue = splitText.length / 2;
+
+  splitText.forEach((e, idx) => {
+    if (idx < halfValue) {
+      cluster += `<span class="start">${e}</span>`;
+    } else {
+      cluster += `<span class ="end">${e}</span>`;
+    }
+  });
+
+  h1.innerHTML = cluster;
+}
+
+breakText();
+
+gsap.from("h1 .start", {
+  y: 90,
+  opacity: 0,
+  delay: 0.5,
+  duration: 0.6,
+  stagger: 0.15,
 });
-
-console.log(cluster);
+gsap.from("h1 .end", {
+  y: 90,
+  opacity: 0,
+  delay: 0.5,
+  duration: 0.6,
+  stagger: -0.15,
+});
